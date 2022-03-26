11954244b4667661e9757cd6f952f0d7cf2f697a
David W Sosa <sosa.david88@gmail.com>
2022-03-26 09:35:50 -0400

    validated xml against dtd

--- dom_parser_dtd.py
+++ dom_parser_dtd.py
@@ -1,24 +1,33 @@
 from lxml import etree
-import xml.etree.ElementTree as ET
+# import xml.etree.ElementTree as ET
 
+# paths to files
 schema_path = "./schema.xml"
 xml_file_path = "./data.xml"
+dtd_file_path = "./dtd.dtd"
 
+# lxml objects
 xml_schema_doc = etree.parse(schema_path)
-
 xml_schema = etree.XMLSchema(xml_schema_doc)
-
 xml_doc  = etree.parse(xml_file_path)
+dtd = etree.DTD(dtd_file_path)
+
 # raise an error is xml does not match schema
 xml_schema.assert_(xml_doc)
 
-#returns true if the xml matches the schema
-result = xml_schema.validate(xml_doc )
+#validate returns true if the xml matches the schema in the schema file
+schema_result = xml_schema.validate(xml_doc)
+print("schema file valid: ", schema_result)
 
-print(result)
+# raise an error is xml does not match dtd
+dtd.assert_(xml_doc)
+
+#validate returns true if the xml matches the dtd in the dtd file
+dtd_result = dtd.validate(xml_doc)
+print("dtd file valid: ", dtd_result)
 
 # iterate over all children and grandchildren of the root element and print corresponding text
-for child in xml_doc.getroot():
-    print(child.tag," : ", child.text)
-    for grandchild in child:
-        print(grandchild.tag ," : ", grandchild.text)
+# for child in xml_doc.getroot():
+#     for grandchild in child:
+#         print(grandchild.tag ," : ", grandchild.text)
+#     print("\n")
