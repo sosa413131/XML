1d20e7a094199a759eef26b08b2f430a3e020d57
David W Sosa <sosa.david88@gmail.com>
2022-03-26 09:05:06 -0400

    updated schema and validated xml against schema

--- dom_parser_dtd.py
+++ dom_parser_dtd.py
@@ -1,8 +1,6 @@
 from lxml import etree
 import xml.etree.ElementTree as ET
 
-# parser = etree.XMLParser(dtd_validation=True)
-
 schema_path = "./schema.xml"
 xml_file_path = "./data.xml"
 
@@ -11,11 +9,16 @@ xml_schema_doc = etree.parse(schema_path)
 xml_schema = etree.XMLSchema(xml_schema_doc)
 
 xml_doc  = etree.parse(xml_file_path)
+# raise an error is xml does not match schema
+xml_schema.assert_(xml_doc)
 
-# result = xml_schema.validate(xml_doc )
-# xml_schema.assert_(xml_doc)
-# print(result)
-print(xml_doc.getroot())
+#returns true if the xml matches the schema
+result = xml_schema.validate(xml_doc )
 
+print(result)
+
+# iterate over all children and grandchildren of the root element and print corresponding text
 for child in xml_doc.getroot():
-    print(child.tag, child.attrib)
+    print(child.tag," : ", child.text)
+    for grandchild in child:
+        print(grandchild.tag ," : ", grandchild.text)
